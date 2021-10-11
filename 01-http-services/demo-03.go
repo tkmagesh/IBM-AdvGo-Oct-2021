12e3d971dc5552e38a210c4115ba176ac458d623
Magesh Kuppan <tkmagesh77@gmail.com>
2021-10-11 12:56:01 +0530

    Using "Chain" to compose middlewares

--- 01-http-services/demo-03.go
+++ 01-http-services/demo-03.go
@@ -6,6 +6,19 @@ import (
 	"time"
 )
 
+type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+func profileTime(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		defer func() {
+			end := time.Now()
+			fmt.Printf("Request took %s\n", end.Sub(start))
+		}()
+		f(w, r)
+	}
+}
+
 func logging(f http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("Request made for : %s\n", r.URL.Path)
@@ -13,9 +26,20 @@ func logging(f http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
+func Chain(f http.HandlerFunc, middlewares ...Middleware) http.HandlerFunc {
+	for _, m := range middlewares {
+		f = m(f)
+	}
+	return f
+}
+
 func main() {
-	http.HandleFunc("/foo", logging(foo))
-	http.HandleFunc("/bar", logging(bar))
+	/*
+		http.HandleFunc("/foo", profileTime(logging(foo)))
+		http.HandleFunc("/bar", profileTime(logging(bar)))
+	*/
+	http.HandleFunc("/foo", Chain(foo, logging, profileTime))
+	http.HandleFunc("/bar", Chain(bar, logging, profileTime))
 	http.ListenAndServe(":8080", nil)
 }
 
