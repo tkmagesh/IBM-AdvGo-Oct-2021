c375f246f7eb0cbc5e1715d5f05fad05e46500ad
Magesh Kuppan <tkmagesh77@gmail.com>
2021-10-12 16:49:36 +0530

    client streaming : client implementation

--- 04-grpc-app/client/client.go
+++ 04-grpc-app/client/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"grpc-app/proto"
 	"log"
+	"sync"
 	"time"
 
 	"google.golang.org/grpc"
@@ -35,26 +36,58 @@ func doRequestResponse(ctx context.Context, client proto.AppServiceClient) {
 }
 
 func doClientStreaming(ctx context.Context, client proto.AppServiceClient) {
-	data := []int32{6, 3, 7, 5, 2, 4, 8, 1, 9}
-	clientStream, err := client.Average(ctx)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	//sending multiple requests to the server
-	for _, val := range data {
-		averageRequest := &proto.AverageRequest{
-			No: val,
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		data := []int32{6, 3, 7, 5, 2, 4, 8, 1, 9}
+		clientStream, err := client.Average(ctx)
+		if err != nil {
+			log.Fatalln(err)
 		}
-		log.Println("Sending: No = ", val)
-		time.Sleep(500 * time.Millisecond)
-		clientStream.Send(averageRequest)
-	}
 
-	//receiving the response from the server
-	res, err := clientStream.CloseAndRecv()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Println("Average : ", res.GetResult())
+		//sending multiple requests to the server
+		for _, val := range data {
+			averageRequest := &proto.AverageRequest{
+				No: val,
+			}
+			log.Println("	Sending (1): No = ", val)
+			time.Sleep(500 * time.Millisecond)
+			clientStream.Send(averageRequest)
+		}
+
+		//receiving the response from the server
+		res, err := clientStream.CloseAndRecv()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Println("	Average (1): ", res.GetResult())
+		wg.Done()
+	}()
+
+	go func() {
+		data := []int32{60, 30, 70, 50, 20, 40, 80, 10, 90}
+		clientStream, err := client.Average(ctx)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		//sending multiple requests to the server
+		for _, val := range data {
+			averageRequest := &proto.AverageRequest{
+				No: val,
+			}
+			log.Println("Sending (2): No = ", val)
+			time.Sleep(250 * time.Millisecond)
+			clientStream.Send(averageRequest)
+		}
+
+		//receiving the response from the server
+		res, err := clientStream.CloseAndRecv()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Println("Average (2): ", res.GetResult())
+		wg.Done()
+	}()
+	wg.Wait()
 }
--- 04-grpc-app/server/server.go
+++ 04-grpc-app/server/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"grpc-app/proto"
+	"io"
 	"log"
 	"net"
 
@@ -21,6 +22,32 @@ func (s *server) Add(ctx context.Context, req *proto.AddRequest) (*proto.AddResp
 		Sum: result,
 	}
 	return response, nil
+}
+
+func (s *server) Average(stream proto.AppService_AverageServer) error {
+	var sum int32
+	var count int32
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			//all the requests are received
+			//prepare the response and send it (use SendAndClose())
+			avg := sum / count
+			response := &proto.AverageResponse{
+				Result: avg,
+			}
+			log.Println("Average Request : sending response result = ", avg)
+			return stream.SendAndClose(response)
+		}
+		if err != nil {
+			return err
+		}
+		no := req.GetNo()
+		log.Println("Average Request : No = ", no)
+		sum = sum + no
+		count++
+	}
+	return nil
 }
 
 func main() {
