228783bc7272e8119c536b335028e9aa8d921f80
Magesh Kuppan <tkmagesh77@gmail.com>
2021-10-13 14:26:48 +0530

    benchmarking & coverage

--- 05-testing/utils/utils.go
+++ 05-testing/utils/utils.go
@@ -1,5 +1,7 @@
 package utils
 
+import "math"
+
 func IsPrime(n int) bool {
 	if n <= 1 {
 		return false
@@ -9,6 +11,55 @@ func IsPrime(n int) bool {
 	}
 	for idx := 2; idx < n; idx++ {
 		if n%idx == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func IsPrime2(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	if n <= 3 {
+		return true
+	}
+	for idx := 2; idx <= (n / 2); idx++ {
+		if n%idx == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func IsPrime3(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	if n <= 3 {
+		return true
+	}
+	var idx float64
+	//nSqrt = math.Sqrt(float64(n))
+	for idx = 2; idx <= math.Sqrt(float64(n)); idx++ {
+		if n%int(idx) == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func IsPrime4(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	if n <= 3 {
+		return true
+	}
+	var idx float64
+	nSqrt := math.Sqrt(float64(n))
+	for idx = 2; idx <= nSqrt; idx++ {
+		if n%int(idx) == 0 {
 			return false
 		}
 	}
